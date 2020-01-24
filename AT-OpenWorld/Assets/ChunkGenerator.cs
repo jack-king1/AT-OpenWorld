3f28532e4d79944e2e225ae17ace071423c4fd5b
King <jack3.king@live.uwe.ac.uk>
2020-01-24 02:49:07 +0000

    working on loading chunks separately from original script

---
+++ AT-OpenWorld/Assets/ChunkGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGenerator : MonoBehaviour
+{
+    [SerializeField]
+    public enum ChunkSize
+    {
+        Six = 6,
+        Nine = 9,
+        Twelve = 12
+    }
+
+    public ChunkSize MapChunkSize;
+
+}
